9b72f22467ed345b5000d0ae9bff262db10533ec
MrSkele <127677552+skelegamerYT11@users.noreply.github.com>
2026-07-12 00:10:04 +0200

    Delete mod_files/Avvia_Connessione.bat

--- mod_files/Avvia_Connessione.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo ==============================================
-echo   LAST CARETAKER - MULTIPLAYER
-echo ==============================================
-powershell -ExecutionPolicy Bypass -File "%~dp0Avvia_Multiplayer.ps1"
-pause
